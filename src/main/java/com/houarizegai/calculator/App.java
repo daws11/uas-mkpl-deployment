6711263303ea668188007b9ee61bf8106cbd63ee
daws11 <96942052+daws11@users.noreply.github.com>
2023-06-14 04:03:33 +0700

    Update App.java

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -1,6 +1,8 @@
 package com.houarizegai.calculator;
 
 import com.houarizegai.calculator.ui.CalculatorUI;
+import com.houarizegai.calculator.ui.CalculatorUI;
+import com.houarizegai.calculator.ui.CalculatorUI;
 
 public class App {
 
